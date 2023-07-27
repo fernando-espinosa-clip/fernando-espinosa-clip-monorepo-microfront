d7b4b7d5c3c660ee5cbbfabd240aa2eeac0a21ca
fernando-espinosa-clip <fernando.espinosa@payclip.com>
2023-07-27 14:51:08 -0600

    WIP

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -30,13 +30,13 @@ const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
-}
+// }
 
 // We are running through container
 // and we should export the mount function
