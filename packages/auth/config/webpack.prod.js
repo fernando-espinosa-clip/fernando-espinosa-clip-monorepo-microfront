f649198c5e0deb60d0df235ddb35da9beddc37d9
fernando-espinosa-clip <fernando.espinosa@payclip.com>
2023-07-27 13:54:14 -0600

    Adding new webpack plugins

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -2,12 +2,15 @@ const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/auth/latest/',
+    publicPath: '/',
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -17,6 +20,21 @@ const prodConfig = {
         './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.png',
+    }),
+    new Dotenv({
+      systemvars: true,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: './',
+        },
+      ],
     }),
   ],
 };
